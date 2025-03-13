744eeffc73709388582a4e63c68ee02b3be60084
IRNAIT <mitrandil15@gmail.com>
2025-03-13 14:33:46 +0300

    chore: обновление конфигурации Next.js для GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
   images: {
     domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com', 'cdn.discordapp.com'],
   },
   basePath: '/IBRIPEDIA',
+  assetPrefix: '/IBRIPEDIA/',
 }
 
 module.exports = nextConfig 
